7cf81a2680f06d3e960648f02c8a05fa7cd69a6a
Ilija Eftimov <ileeftimov@gmail.com>
2017-12-07 12:30:02 +0100

    Add more granularity to client timeouts configurations

--- lib/aftership/v4/configuration.rb
+++ lib/aftership/v4/configuration.rb
@@ -1,19 +1,25 @@
 module AfterShip
   module V4
     class Configuration
-      attr_accessor :api_endpoint, :api_key
-      attr_writer :timeout
+      attr_writer :connect_timeout, :send_timeout, :receive_timeout
 
       def initialize
         @api_endpoint = 'https://api.aftership.com'
-        @timeout = 3
         @headers = { 'Content-Type' => 'application/json' }
+        @connect_timeout = @receive_timeout = 500 # milliseconds
+        @send_timeout = 1000 # milliseconds
       end
 
-      def timeout
-        return 0 unless @timeout
+      def connect_timeout
+        to_seconds(@connect_timeout)
+      end
 
-        @timeout.to_f / 1000
+      def send_timeout
+        to_seconds(@send_timeout)
+      end
+
+      def receive_timeout
+        to_seconds(@receive_timeout)
       end
 
       def headers
@@ -22,6 +28,12 @@ module AfterShip
 
       def headers= h
         @headers.merge!(h)
+      end
+
+      private
+
+      def to_seconds(milliseconds)
+        milliseconds.to_f / 1000
       end
     end
   end
